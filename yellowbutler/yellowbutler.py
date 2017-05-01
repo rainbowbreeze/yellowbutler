8576774d10a7a66c7a19a72e0d49a684dd5768bf
rainbowbreeze <rainbowbreeze@gmx.com>
2017-05-01 23:21:34 +0200

    Added the echo message api draft

--- yellowbutler/yellowbutler.py
+++ yellowbutler/yellowbutler.py
@@ -10,7 +10,27 @@ class YellowButler:
     """
 
     def __init__(self):
-        pass
+        # Registers all the allowed keys
+        self.keys = []
+        # TODO: Registers Yellow Butler modules to add features
 
-    def get_message(self):
-        return "YellowButler 1.0 is here!"
+    def echo_message(self, key, message):
+        """Echo a simple message"""
+        # if not self.is_security_code_valid(key):
+        #     return self.unauthorize_answer(key)
+        return 'You said: {}'.format(message)
+
+    def append_security_key(self, new_key):
+        """Add a new security key to the allowed ones"""
+        self.keys.append(new_key)
+
+    def is_security_code_valid(self, security_code):
+        try:
+            self.keys.index(security_code)
+            return True
+        except ValueError:
+            return False
+
+    def unauthorize_answer(self, key):
+        print("Attempt to access with a wrong key {}".format(key))
+        return 404
