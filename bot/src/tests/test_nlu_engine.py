c57ad6af5ee061a866b734bbdb3fa24cff3365af
rainbowbreeze <r@r.com>
2017-12-25 19:50:27 +0100

    added more tests for echo gear

--- bot/src/tests/test_nlu_engine.py
+++ bot/src/tests/test_nlu_engine.py
@@ -19,3 +19,11 @@ class TestNluEngine(object):
         assert intent == GlobalBag.ECHO_MESSAGE_INTENT
         assert GlobalBag.ECHO_MESSAGE_PARAM_MESSAGE in params
         assert params.get(GlobalBag.ECHO_MESSAGE_PARAM_MESSAGE) == "very long message"
+        intent, params = nlu_engine.infer_intent_and_args("Say whatever you want!")
+        assert intent == GlobalBag.ECHO_MESSAGE_INTENT
+        assert GlobalBag.ECHO_MESSAGE_PARAM_MESSAGE in params
+        assert params.get(GlobalBag.ECHO_MESSAGE_PARAM_MESSAGE) == "whatever you want!"
+        intent, params = nlu_engine.infer_intent_and_args("Repeat a sentence of your choice")
+        assert intent == GlobalBag.ECHO_MESSAGE_INTENT
+        assert GlobalBag.ECHO_MESSAGE_PARAM_MESSAGE in params
+        assert params.get(GlobalBag.ECHO_MESSAGE_PARAM_MESSAGE) == "a sentence of your choice"
