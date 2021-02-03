8fc374e8c9446a1b1c18c7a14d30aa1da38e73cb
rainbowbreeze <381940+rainbowbreeze@users.noreply.github.com>
2021-02-03 12:08:02 +0100

    Test in place for NewsReportGear

--- bot/src/tests/yellowbot/gears/test_newsreportgear.py
+++ bot/src/tests/yellowbot/gears/test_newsreportgear.py
@@ -1,5 +1,7 @@
-"""
-Test NewsReport gear
+"""Test NewsReportGear
+
+Responses library is used to mock Request calls
+https://github.com/getsentry/responses
 """
 
 from unittest import TestCase
@@ -11,13 +13,70 @@ from yellowbot.gears.newsreportergear import NewsReportGear
 from yellowbot.globalbag import GlobalBag
 
 class TestNewsReportGear(TestCase):
+    TESTDATA_YOUTUBE_CHANNEL_FILENAME = os.path.join(os.path.dirname(__file__), "testdata_youtube_channel.txt")
+    TESTDATA_YOUTUBE_PLAYLIST_FILENAME = os.path.join(os.path.dirname(__file__), "testdata_youtube_playlist.txt")
+
     def setUp(self):
-        self._gear = NewsReportGear()
+        self._youtube_key = 'mock_youtube_key'
+        self._gear = NewsReportGear(self._youtube_key)
 
     def tearDown(self):
         pass
 
-    def test_youtube(self):
-        result = self._gear._analize_youtube_channel('')
-        self.assertEqual(2, len(result))
+    @responses.activate
+    def test_youtube_upload_playlist_from_channel(self):
+        testdata = open(self.TESTDATA_YOUTUBE_CHANNEL_FILENAME).read()
 
+        # For channel_id = 'UCSbdMXOI_3HGiFviLZO6kNA'
+        # expected upload_playlist_id = 'UUSbdMXOI_3HGiFviLZO6kNA' # Slightly different
+
+        responses.add(
+            responses.GET,
+            'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&id=UCSbdMXOI_3HGiFviLZO6kNA&key=mock_youtube_key',
+            body = testdata,
+            status = 200,
+            content_type='application/json'
+        )
+
+        result = self._gear._youtube_find_upload_playlist_from_channel(self._youtube_key, 'UCSbdMXOI_3HGiFviLZO6kNA')
+        self.assertEqual('UUSbdMXOI_3HGiFviLZO6kNA', result)
+    
+    @responses.activate
+    def test_youtube_find_new_videos_in_a_playlist(self):
+        testdata = open(self.TESTDATA_YOUTUBE_PLAYLIST_FILENAME).read()
+
+        responses.add(
+            responses.GET,
+            'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=UUSbdMXOI_3HGiFviLZO6kNA&key=mock_youtube_key',
+            body = testdata,
+            status = 200,
+            content_type='application/json'
+        )
+
+        results = self._gear._youtube_find_new_videos_in_a_playlist(self._youtube_key, 'UUSbdMXOI_3HGiFviLZO6kNA')
+        self.assertEqual(10, len(results))
+
+        item = results[0]
+        self.assertEqual('https://www.youtube.com/watch?v=WFeny7l1Ev4', item.url)
+        self.assertEqual('The 7 Types of VR Users 2', item.title)
+        self.assertEqual('2021-02-02T19:15:04Z', item.published)
+
+        item = results[1]
+        self.assertEqual('https://www.youtube.com/watch?v=veVx0AuhHFw', item.url)
+        self.assertEqual('Valve\'s next VR projects are SCARILY similar to Sword Art Online', item.title)
+        self.assertEqual('2021-01-27T20:00:10Z', item.published)
+
+        item = results[2]
+        self.assertEqual('https://www.youtube.com/watch?v=lILlWMLTn0c', item.url)
+        self.assertEqual('What Happened to my Valve Index After 2000 Hours?', item.title)
+        self.assertEqual('2021-01-23T19:15:01Z', item.published)
+
+        item = results[3]
+        self.assertEqual('https://www.youtube.com/watch?v=EzHjucDrNsY', item.url)
+        self.assertEqual('The Oculus Quest 2 gets a HUGE Update for QoL', item.title)
+        self.assertEqual('2021-01-19T19:35:57Z', item.published)
+
+        item = results[4]
+        self.assertEqual('https://www.youtube.com/watch?v=PIIXyfuxOcU', item.url)
+        self.assertEqual('CES 2021 brings INSANE new VR Technology', item.title)
+        self.assertEqual('2021-01-12T19:13:35Z', item.published)
--- bot/src/yellowbot/gears/newsreportergear.py
+++ bot/src/yellowbot/gears/newsreportergear.py
@@ -1,6 +1,9 @@
-"""
-A YellowBot gear to check for news from different sources, and send them over
- a surface
+"""A YellowBot gear to check for news from different sources, and send them over a surface.
+
+This class controls various source of information, like RSS feeds, YouTube
+ channels, misc pages, etc, and checks if there are updated content since the
+ last check.
+Once updates are found, they're send over a surface to notify the user.
 
 Requirements
 - requests
@@ -17,20 +20,25 @@ from yellowbot.globalbag import GlobalBag
 from yellowbot.loggingservice import LoggingService
 
 class NewsReportGear(BaseGear):
+    """Check updates from different sources and send them to a specific surface
     """
-    Check updates from different sources and send them to a specific surface
 
-    """
     INTENTS = [GlobalBag.CHECKFORNEWS_INTENT]
     # PARAM_SILENT = GlobalBag.COMMITSTRIP_PARAM_SILENT  # No notification if there is nothing new 
 
     def __init__(self,
+                 youtube_api_key,
                  test_mode = False):
+        """Constructor
+
+        :param youtube_api_key: the API key to use for YouTube API v3 calls
+        :type api_key: str 
         """
-        """
+
         BaseGear.__init__(self, NewsReportGear.__name__, self.INTENTS)
         self._logger = LoggingService.get_logger(__name__)
         self._test_mode = test_mode
+        self._youtube_api_key = youtube_api_key
 
     def process_intent(self, intent, params):
         if NewsReportGear.INTENTS[0] != intent:
@@ -46,21 +54,18 @@ class NewsReportGear(BaseGear):
         return self._analize_youtube_channel('')
 
     def _analyze_and_notify_news_sources(self):
-        """
-        Analyze all the different news sources, notifying in case new contents
-         are found
+        """Analyze all the different news sources, notifying in case new
+         contents are found
         """
 
         # Based on the source address, it calls a different logic
         return
     
     def _analize_youtube_channel(self, youtube_channel_id):
-        """
-        Analyze a YouTube channel searching for new videos
+        """Analyze a YouTube channel searching for new videos
 
         :param youtube_channel_id: the id of the channel to analyze
         :type youtube_channel_id: str
-
         """
 
         # Every channel as a special playlist called upload, with all the 
@@ -72,21 +77,31 @@ class NewsReportGear(BaseGear):
         #  the channel, cache it, and then search for new videos inside
         #  this playlist
 
-
         # Search in the database if it has already the "upload" playlist id 
         #  for the given channel.
         # This pla
+        upload_playlist_id = self._youtube_find_upload_playlist_from_channel(self._youtube_api_key, channel_id)
+        self._youtube_find_new_videos_in_a_playlist(self._youtube_api_key, upload_playlist_id)
 
         # Find the upload playlist id for the given channel
 
         # Search latest videos in the upload playlist
 
-        channel_id = 'UCSbdMXOI_3HGiFviLZO6kNA'
-        upload_id = 'UUSbdMXOI_3HGiFviLZO6kNA' # Slightly different
-        api_key = 'AIzaSyD0es8hfz0z85ZAP5CM5k6o9pLtX6SgpyI'
+    def _youtube_find_upload_playlist_from_channel(self, api_key, channel_id):
+        """Find upload playlist id for a given channel
 
-        url = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=2&playlistId={}&key={}'.format(
-            upload_id,
+        :param api_key: the API key to use for YouTube API v3 calls
+        :type api_key: str
+
+        :param channel_id: the id of the channel where the search is performed
+        :type channel_id: str
+
+        :returns: the id of the special "upload" playlist
+        :rtype: str
+        """
+
+        url = 'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&id={}&key={}'.format(
+            channel_id,
             api_key
         )
 
@@ -97,9 +112,48 @@ class NewsReportGear(BaseGear):
             results = req.json()
         except BaseException as e:
             self._logger.exception(e)
-            return "Error while getting playlist information for channel {} and playlist {}: {}".format(
+            return "Error while getting information for channel {}: {}".format(
                 channel_id,
-                upload_id,
+                repr(e)
+            )
+
+        upload_id = None
+        try:
+            channel_items = results['items']
+            upload_id = channel_items[0]['contentDetails']['relatedPlaylists']['uploads']
+        except BaseException as e:
+            return "Exception happened while parsing YouTube data {}".format(repr(e))        
+
+        return upload_id
+
+
+    def _youtube_find_new_videos_in_a_playlist(self, api_key, playlist_id):
+        """Given a playlist, it searched for its latest videos
+
+        :param api_key: the API key to use for YouTube API v3 calls
+        :type api_key: str
+
+        :param playlist_id: the id of the playlist
+        :type playlist_id: str
+
+        :returns: a collection of objects, representing videos
+        :rtype: list
+        """
+
+        url = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId={}&key={}'.format(
+            playlist_id,
+            api_key
+        )
+
+        try:
+            req = requests.get(url)
+            if not req.ok:
+                req.raise_for_status()
+            results = req.json()
+        except BaseException as e:
+            self._logger.exception(e)
+            return "Error while getting playlist information for playlist {}: {}".format(
+                playlist_id,
                 repr(e)
             )
 
@@ -122,5 +176,8 @@ class NewsReportGear(BaseGear):
             return "Exception happened while parsing YouTube data {}".format(repr(e))        
 
         return latest_videos
+
+
+
 
 
